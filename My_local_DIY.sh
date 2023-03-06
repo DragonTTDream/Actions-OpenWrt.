72e6e53dbdd8a25d654680a7966abb2a97f90e56
DragonTTDream <87249980+DragonTTDream@users.noreply.github.com>
2023-03-06 17:53:19 +0800

    Update My_local_DIY.sh

--- My_local_DIY.sh
+++ My_local_DIY.sh
@@ -24,7 +24,7 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/
 #sed -i "s|s9xxx_lede|ARMv8|g" package/luci-app-amlogic/root/etc/config/amlogic
 
 #Turbo ACC
-#ssvn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shortcut-fe package/shortcut-fe
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shortcut-fe package/shortcut-fe
 
 
 ./scripts/feeds update -a
